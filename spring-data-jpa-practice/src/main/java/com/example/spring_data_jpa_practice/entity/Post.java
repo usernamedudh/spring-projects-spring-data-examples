6ea086d6a007706f08c516d3737c8d5a2af8a8fd
vampwave <vampwave2@gmail.com>
2025-11-29 00:11:00 +0200

    Add entity classes

---
+++ spring-data-jpa-practice/src/main/java/com/example/spring_data_jpa_practice/entity/Post.java
@@ -0,0 +1,25 @@
+package com.example.spring_data_jpa_practice.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "posts")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
---
+++ spring-data-jpa-practice/src/main/java/com/example/spring_data_jpa_practice/entity/User.java
@@ -0,0 +1,26 @@
+package com.example.spring_data_jpa_practice.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Post> posts;
+}
