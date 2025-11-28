a082f33b5ed19e485ca56393535ae00b1bdb20dd
vampwave <vampwave2@gmail.com>
2025-11-29 00:13:28 +0200

    Add entire project

---
+++ spring-data-jpa-practice/src/main/java/com/example/spring_data_jpa_practice/SpringDataJpaPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_data_jpa_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaPracticeApplication.class, args);
+	}
+
+}
